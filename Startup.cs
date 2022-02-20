cf96a91fb5a4374e033c48c829c619050ba61f4d
Andrew <21085039+asmarsh@users.noreply.github.com>
2022-02-19 22:51:23 -0500

    End of Chapter 17

--- Startup.cs
+++ Startup.cs
@@ -1,22 +1,49 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Platform.Models;
+using Platform.Services;
 
 namespace Platform
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration config)
         {
-            services.AddDistributedMemoryCache(opts =>
-            {
-                opts.SizeLimit = 200;
-            });
+            Configuration = config;
         }
 
-        public void Configure(IApplicationBuilder app)
+        private IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDistributedSqlServerCache(opts =>
+            {
+                opts.ConnectionString
+                = Configuration["ConnectionStrings:CacheConnection"];
+                opts.SchemaName = "dbo";
+                opts.TableName = "DataCache";
+            });
+            services.AddResponseCaching();
+            services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
+            services.AddDbContext<CalculationContext>(opts =>
+            {
+                opts.UseSqlServer(Configuration["ConnectionStrings:CalcConnection"]);
+                opts.EnableSensitiveDataLogging(true);
+            });
+            services.AddTransient<SeedData>();
+        }
+
+        public void Configure(IApplicationBuilder app,
+        IHostApplicationLifetime lifetime, IWebHostEnvironment env,
+        SeedData seedData)
         {
             app.UseDeveloperExceptionPage();
+            app.UseResponseCaching();
             app.UseStaticFiles();
             app.UseRouting();
             app.UseEndpoints(endpoints =>
@@ -27,6 +54,15 @@ namespace Platform
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+            bool cmdLineInit = (Configuration["INITDB"] ?? "false") == "true";
+            if (env.IsDevelopment() || cmdLineInit)
+            {
+                seedData.SeedDatabase();
+                if (cmdLineInit)
+                {
+                    lifetime.StopApplication();
+                }
+            }
         }
     }
 }
--- SumEndpoint.cs
+++ SumEndpoint.cs
@@ -1,31 +1,30 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Caching.Distributed;
+using Platform.Models;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Platform
 {
     public class SumEndpoint
     {
-        public async Task Endpoint(HttpContext context, IDistributedCache cache)
+        public async Task Endpoint(HttpContext context,
+        CalculationContext dataContext)
         {
             int count = int.Parse((string)context.Request.RouteValues["count"]);
-            string cacheKey = $"sum_{count}";
-            string totalString = await cache.GetStringAsync(cacheKey);
-            if (totalString == null)
+            long total = dataContext.Calculations
+            .FirstOrDefault(c => c.Count == count)?.Result ?? 0;
+            if (total == 0)
             {
-                long total = 0;
                 for (int i = 1; i <= count; i++)
                 {
                     total += i;
                 }
-                totalString = $"({ DateTime.Now.ToLongTimeString() }) {total}";
-                await cache.SetStringAsync(cacheKey, totalString,
-                new DistributedCacheEntryOptions
-                {
-                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(2)
-                });
+                dataContext.Calculations!
+                .Add(new Calculaton() { Count = count, Result = total });
+                await dataContext.SaveChangesAsync();
             }
+            string totalString = $"({ DateTime.Now.ToLongTimeString() }) {total}";
             await context.Response.WriteAsync(
             $"({DateTime.Now.ToLongTimeString()}) Total for {count}"
             + $" values:\n{totalString}\n");
